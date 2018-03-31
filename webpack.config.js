3e75cacb5e33d6719546fa28388716bec58fe6da
Kevin Miller <kevin@driftabout.com>
2018-03-31 14:21:29 -0700

    fixed bug in webpack rules

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        use: [
+          { 
+            loader: 'babel-loader',
+          },
+        ],
       },
       {
         test: /\.scss$/,
